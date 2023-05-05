29443afc4cd66a4404bbf06b30b5a5baa98f1728
Jacob S. Hataway <hatawajs@dukes.jmu.edu>
2023-05-05 17:19:14 -0400

    .

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let redirectUri = "https://jacobhataway.github.io/343-p3/" //"https://w3stu.cs.j
 console.log("HI")
 const urlParams = new URLSearchParams(window.location.search);
 let code = urlParams.get('code');
-let token = localStorage.getItem('access_token')
+let token = null;//localStorage.getItem('access_token')
 console.log(token, code);
 //console.log(code);
 
